a81292c0e6b7a07fd3ba4d3c28a88307f8f05251
thuong <hathuong085@gmail.com>
2020-08-01 18:01:39 +0700

    Cập nhật 18h 01/08/2020

---
+++ Cimena.API/Controllers/ShowingController.cs
@@ -0,0 +1,28 @@
+﻿using Cimena.BAL.INTERFACE;
+using Cimena.Domain.Responses.Showing;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cimena.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ShowingController
+    {
+        private readonly IShowingService showingService;
+
+        public ShowingController(IShowingService showingService)
+        {
+            this.showingService = showingService;
+        }
+        [HttpDelete]
+        [Route("/api/Showing/DeleteByTime/")]
+        public async Task<MessageSuccess> DeleteFilm()
+        {
+            return await showingService.DeleteShowingByTime();
+        }
+    }
+}
---
+++ Cimena.DAL/ShowingRepository.cs
@@ -0,0 +1,20 @@
+﻿using Cimena.DAL.INTERFACE;
+using Cimena.Domain.Responses.Showing;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cimena.DAL
+{
+    public class ShowingRepository : BaseRepository,IShowingRepository
+    {
+
+        public async Task<MessageSuccess> DeleteShowingByTime()
+        {
+            return await SqlMapper.QueryFirstOrDefaultAsync<MessageSuccess>(conn, "sp_deleteshowedsByTimes", CommandType.StoredProcedure);
+        }
+    }
+}
